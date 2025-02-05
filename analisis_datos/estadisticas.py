a7d4daa94fc99dca26f7c8f20208db45ae6b49f5
Gerald Daniel Chaves Chacón. <57466973+Geraldch@users.noreply.github.com>
2025-02-04 21:09:17 -0600

    Sesion 5

--- analisis_datos/estadisticas.py
+++ analisis_datos/estadisticas.py
@@ -1,1 +1,22 @@
 # estadisticas.py
+def media(datos):
+    #Este es un ejemplo de documentación, se activa poniendo """+TAB y se escoge args
+    """Ésta función calcúla la media aritmética de una lista con valores numéricos.
+
+    Args:
+        datos (Lista): Lista de numeros
+
+    Returns:
+        Float: Flotante de la media aritmetica
+    """
+    
+    return sum(datos) / len(datos)
+
+def calcular_mediana(datos):
+    datos_sorted = sorted(datos)
+    n = len(datos)
+    mid = n // 2
+    if n % 2 == 0:
+        return (datos_sorted[mid - 1] + datos_sorted[mid]) / 2.0
+    else:
+        return datos_sorted[mid]
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-# Proyecto: [Nombre del Proyecto]
-# Estudiante: [Nombre del Estudiante]
-# Fecha de Inicio: [dd/mm/aaaa]
+# Proyecto: Sesión 5
+# Estudiante: Gerald D. Chaves.
+# Fecha de Inicio: 2025/02/24
 # Fecha de Entrega: [dd/mm/aaaa]
 # Descripción: Este archivo contiene el punto de entrada principal del proyecto.
 # Recuerda incluir tu nombre completo, la fecha en la que iniciaste el proyecto y la fecha estimada de entrega.
 # Esto ayuda a mantener un registro claro del trabajo realizado.
+
+from analisis_datos.estadisticas import media, calcular_mediana
+
+
+lista_parametro  = [5,3,1,2,4]
+print('Media: ', media(lista_parametro))
+print('Mediana: ', calcular_mediana(lista_parametro))
